dc1657ccaf6fd4d106deefd82ca3abc88b39f165
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-03-30 12:27:02 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+// Variable multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Variable random that will generate a random integer greater than 0.
+let random = Math.ceil(Math.random() * 100); // Generates a random integer between 1 and 100
+
+// Variable mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+// Variable max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max(5, 10, 15, 20);
+
+// Logging the variables to check the values
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
